07ffddbe371834a3349f209335f0226c1ad593de
Vibe-NLP <77207615+Vibe-NLP@users.noreply.github.com>
2021-10-31 10:56:07 +0100

    Update NP_disj_parser.py

--- NP_disj_parser.py
+++ NP_disj_parser.py
@@ -9,17 +9,13 @@ doc = nlp(introduction_file_text)
 
 sentences=[]
 conjunction=[]
-count=0
 
 for sent in doc.sents:
-        count+=1
         for token in sent:
-               # if token.text.lower() in ("or","and/or","and\or","and") and token.dep_=="cc":
-               if token.text.lower() in ("or") and token.dep_=="cc":
+               if token.text.lower() in ("or","and/or","and\or") and token.dep_=="cc":
                        if token.head.pos_ in("NOUN","ADP"):
                         conjunction.append(token.text)
                         sentences.append(sent.text)
-print(count)
 print("Total matches found:", len(conjunction))
 
 for match_conjunction, match_sentences  in zip(conjunction,sentences):
