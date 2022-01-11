cebd4806c17cb7d61d52b8a51e26d13a2837afeb
Vibe-NLP <77207615+Vibe-NLP@users.noreply.github.com>
2022-01-11 17:26:10 +0100

    Update lexical_analyser.py

--- lexical_analyser.py
+++ lexical_analyser.py
@@ -1,14 +1,21 @@
 from numpy import string_
 import spacy
 from spacy.matcher import PhraseMatcher
+#To apply the trained pipeline, we load it. 
+#This will assign to nlp an object of type Language containing all the components and data 
+#needed to process the text string, usually called "text". 
 nlp = spacy.load("en_core_web_sm")
 print ("Which file do you want to analyse?")
 reqdoc = string_(input()) 
 file_name = reqdoc
 introduction_file_text = open(file_name).read()
+#we call the nlp object on the text to be analysed, this will return an oject, 
+#called doc, that contains all the generated information
 doc = nlp(introduction_file_text)
 
-# Dizionario
+
+
+# Choice of the dictionary 
 print ("Which dictionary do you want to use?")
 dict = string_(input()) 
 dictionary_name= dict
