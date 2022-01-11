07af2b8f204e58387c516f8ba631cf91ea67de16
Vibe-NLP <77207615+Vibe-NLP@users.noreply.github.com>
2022-01-11 17:29:59 +0100

    Update NP_disj_parser.py

--- NP_disj_parser.py
+++ NP_disj_parser.py
@@ -1,10 +1,18 @@
 import spacy
 from spacy.symbols import NOUN, cc
 from spacy import displacy
+#To apply the trained pipeline, we load it. 
+#This will assign to nlp an object of type Language containing all the components and data 
+#needed to process the text string. 
 nlp = spacy.load("en_core_web_sm")
-file_name = 'C:/Users/Laura/Documents/Articoli/2021spaCy/smartHouse.txt'
+print ("Which file do you want to analyse?")
+reqdoc = string_(input()) 
+file_name = reqdoc
 introduction_file_text = open(file_name).read()
+#we call the nlp object on the text to be analysed, this will return an oject, 
+#called doc, that contains all the generated information
 doc = nlp(introduction_file_text)
+#optional, uncomment to display the syntactic relations
 #spacy.displacy.serve(doc, style='dep')
 
 sentences=[]
